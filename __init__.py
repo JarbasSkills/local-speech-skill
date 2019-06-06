c78ca36e94adbcc80327815a5061dc646dabca29
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2019-06-06 16:34:56 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,30 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+from mycroft.util import create_daemon
+from mycroft.util.signal import create_signal
+from mycroft.messagebus.message import Message
+from kaldi_spotter import KaldiWWSpotter
+import json
 
 class LocalSpeech(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+    
+    def initialize(self):
+        self.kaldi = KaldiWWSpotter()
+        self.kaldi.on("hotword", self.handle_hotword)
+        create_daemon(self.kaldi.run)
 
-    @intent_file_handler('speech.local.intent')
-    def handle_speech_local(self, message):
-        self.speak_dialog('speech.local')
-
-
+    def handle_hotword(self, event):
+        event = json.loads(event)
+        data = event["data"]
+        if data["intent"] == "listen":
+            self.bus.emit(Message('recognizer_loop:wakeword', data))
+            create_signal('startListening')
+        else:
+            data["utterances"] = [data["utterance"]]
+            data.pop["utterance"]
+            self.bus.emit(Message('recognizer_loop:utterance', data))
+        
 def create_skill():
     return LocalSpeech()
 
