b9780ce9ce15da62ee2f041e57744c0df2d45128
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2019-06-06 18:31:33 +0100

    dynamic addition of new hotwords
    TODO settings.meta

--- __init__.py
+++ __init__.py
@@ -6,14 +6,39 @@ from kaldi_spotter import KaldiWWSpotter
 import json
 
 class LocalSpeech(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
-    
     def initialize(self):
+        self.settings.set_changed_callback(self.handle_settings_changed)
+        if "hotwords" not in self.settings:
+            self.settings["hotwords"] = {}
+            
         self.kaldi = KaldiWWSpotter()
         self.kaldi.on("hotword", self.handle_hotword)
+        
+        for hw in self.settings["hotwords"]:
+            self.kaldi.add_hotword(hw, self.settings["hotwords"][hw])
         create_daemon(self.kaldi.run)
 
+    def handle_settings_changed(self):
+        # dynamic update of words
+        if self.settings.get("new_word"):
+            name = self.settings["new_word"]
+            self.settings["hotwords"][name] = {"transcriptions": [name], "intent": "listen", "sensitivity": s}
+        
+        if self.settings.get("delete_word"):
+            name = self.settings["delete_word"]
+            if name in self.settings["hotwords"]:
+                self.settings["hotwords"].pop(name)
+        
+        # remove words if deleted
+        for hw in self.kaldi.hotwords:
+            if hw not in self.settings["hotwords"]:
+                self.kaldi.remove_hotword(hw)
+                
+        # add new words
+        for hw in self.settings["hotwords"]:
+            if hw not in self.kaldi.hotwords:
+                self.kaldi.add_hotword(hw, self.settings["hotwords"][hw])
+    
     def handle_hotword(self, event):
         event = json.loads(event)
         data = event["data"]
